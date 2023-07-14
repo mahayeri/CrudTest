0631e8be687068d9c6a09ccdd2efa3f620daaeff
mahaery <mohammadali.haery@geeks.ltd.uk>
2023-07-14 15:36:00 +0100

    added the repository implementation

---
+++ src/CrudTest.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,42 @@
+﻿using CrudTest.Application.Common.Interfaces.Persistence;
+using CrudTest.Domain.CustomerAggregate;
+using CrudTest.Domain.CustomerAggregate.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudTest.Infrastructure.Persistence.Repositories;
+public sealed class CustomerRepository : ICustomerRepository
+{
+	private readonly ApplicationDbContext _context;
+
+	public CustomerRepository(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<Customer?> GetByIdAsync(CustomerId customerId)
+	{
+		Customer? customer = await _context.Customers
+			.FirstOrDefaultAsync(c => c.Id == customerId);
+		return customer;
+	}
+
+	public async Task Add(Customer customer)
+	{
+		await _context.AddAsync(customer);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task Update(Customer customer)
+	{
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task Delete(CustomerId customerId)
+	{
+		Customer? customer = await _context.Customers.FirstOrDefaultAsync(c => c.Id == customerId);
+
+		if (customer is null) return;
+
+		_context.Customers.Remove(customer);
+	}
+}
