de296e9dcaf0f20db6a0f33fda9069a6cfeb4f41
mamba <m.a.haery110@gmail.com>
2023-07-14 21:52:34 +0330

    added errors controller

---
+++ src/CrudTest.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudTest.Api.Controllers;
+
+[ApiController]
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public IActionResult Error()
+    {
+        return Problem();
+    }
+}
