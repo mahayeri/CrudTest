6bff0a86c9a094918a6bee99d759130766a82cb8
mahaery <mohammadali.haery@geeks.ltd.uk>
2023-07-14 15:31:24 +0100

    added customer database configuration

---
+++ src/CrudTest.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using CrudTest.Domain.CustomerAggregate;
+using CrudTest.Domain.CustomerAggregate.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CrudTest.Infrastructure.Persistence.Configurations;
+public sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+	public void Configure(EntityTypeBuilder<Customer> builder)
+	{
+		builder.HasKey(c => c.Id);
+
+		builder.Property(c => c.Id)
+			.ValueGeneratedNever()
+			.HasConversion(
+				id => id.Value,
+				value => CustomerId.Create(value));
+
+		builder.Property(c => c.FirstName)
+			.IsRequired()
+			.HasMaxLength(50);
+
+		builder.Property(c => c.LastName)
+			.IsRequired()
+			.HasMaxLength(50);
+
+		builder.Property(c => c.Email)
+			.IsRequired()
+			.HasMaxLength(50);
+
+		builder.HasIndex(
+				c => new
+				{
+					c.FirstName,
+					c.LastName,
+					c.DateOfBirth
+				})
+			.IsUnique();
+
+		builder.HasIndex(c => c.Email)
+			.IsUnique();
+
+		builder.Property(c => c.PhoneNumber)
+			.IsRequired()
+			.HasConversion(
+				str => PhoneNumberAsULong(str),
+				u64 => ULongAsPhoneNumber(u64));
+
+	}
+
+	public static ulong PhoneNumberAsULong(string phoneNumber)
+	{
+		var plusRemoved = phoneNumber[1..];
+		var u64 = ulong.Parse(plusRemoved);
+		return u64;
+	}
+
+	public static string ULongAsPhoneNumber(ulong u64)
+	{
+		var plusAdded = $"+{u64}";
+		return plusAdded;
+	}
+}
