98100e0d8dc70ba610977dbbd2394a3500ed1fdd
mahaery <mohammadali.haery@geeks.ltd.uk>
2023-07-14 06:30:51 +0100

    added customer aggregate root

---
+++ src/CrudTest.Domain/CustomerAggregate/Customer.cs
@@ -0,0 +1,47 @@
+﻿using CrudTest.Domain.Common.Models;
+using CrudTest.Domain.CustomerAggregate.ValueObjects;
+
+namespace CrudTest.Domain.CustomerAggregate;
+public sealed class Customer : AggregateRoot<CustomerId>
+{
+	public string FirstName { get; private set; }
+	public string LastName { get; private set; }
+	public DateTime DateOfBirth { get; private set; }
+	public string PhoneNumber { get; private set; }
+	public string Email { get; private set; }
+	public string BankAccountNumber { get; private set; }
+
+	private Customer(
+		CustomerId id,
+		string firstName,
+		string lastName,
+		DateTime dateOfBirth,
+		string phoneNumber,
+		string email,
+		string bankAccountNumber) : base(id)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		DateOfBirth = dateOfBirth;
+		PhoneNumber = phoneNumber;
+		Email = email;
+		BankAccountNumber = bankAccountNumber;
+	}
+	public static Customer Create(
+		string firstName,
+		string lastName,
+		DateTime dateOfBirth,
+		string phoneNumber,
+		string email,
+		string bankAccountNumber)
+	{
+		return new(
+			CustomerId.CreateUnique(),
+			firstName,
+			lastName,
+			dateOfBirth,
+			phoneNumber,
+			email,
+			bankAccountNumber);
+	}
+}
---
+++ src/CrudTest.Domain/CustomerAggregate/ValueObjects/CustomerId.cs
@@ -0,0 +1,14 @@
+﻿using CrudTest.Domain.Common.Models;
+
+namespace CrudTest.Domain.CustomerAggregate.ValueObjects;
+
+public sealed class CustomerId : ValueObject
+{
+	public Guid Value { get; }
+	private CustomerId(Guid value) => Value = value;
+	public static CustomerId CreateUnique() => new(Guid.NewGuid());
+	public override IEnumerable<object> GetEqualityComponents()
+	{
+		yield return Value;
+	}
+}
