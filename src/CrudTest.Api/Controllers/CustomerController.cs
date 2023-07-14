a078e915c1068162f619fd73315128a2308a9f38
mamba <m.a.haery110@gmail.com>
2023-07-14 22:01:50 +0330

    added customer controller added contracts project

---
+++ src/CrudTest.Api/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using CrudTest.Contracts.Customers;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudTest.Api.Controllers;
+
+[Route("[controller]")]
+public class CustomerController : ApiController
+{
+    private readonly IMapper _mapper;
+
+    public CustomerController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCustomer(
+        CreateCustomerRequest request,
+        CancellationToken ct)
+    {
+
+        return Ok();
+    }
+}
---
+++ src/CrudTest.Contracts/Customers/CustomerResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudTest.Contracts.Customers;
+public sealed record CustomerResponse(
+    string Id,
+    string FirstName,
+    string LastName,
+    DateTime DateOfBirth,
+    string PhoneNumber,
+    string Email,
+    string BankAccountNumber);
