5a6461bcd6d0ce18300a49988c8eb3a5776b3d23
mahaery <mohammadali.haery@geeks.ltd.uk>
2023-07-14 15:29:36 +0100

    added db context

---
+++ src/CrudTest.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using CrudTest.Domain.Common.Models;
+using CrudTest.Domain.CustomerAggregate;
+using CrudTest.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CrudTest.Infrastructure.Persistence;
+
+public sealed class ApplicationDbContext : DbContext
+{
+	private readonly PublishDomainEventsInterceptor _publishDomainEventsInterceptor;
+	public ApplicationDbContext(
+		DbContextOptions<ApplicationDbContext> options, PublishDomainEventsInterceptor publishDomainEventsInterceptor)
+		: base(options)
+	{
+		_publishDomainEventsInterceptor = publishDomainEventsInterceptor;
+	}
+
+	public DbSet<Customer> Customers => Set<Customer>();
+
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder
+			.Ignore<List<IDomainEvent>>()
+			.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+		base.OnModelCreating(builder);
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.AddInterceptors(_publishDomainEventsInterceptor);
+		base.OnConfiguring(optionsBuilder);
+	}
+
+	public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+	{
+		return await base.SaveChangesAsync(cancellationToken);
+	}
+}
