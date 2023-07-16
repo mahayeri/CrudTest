1b0968d8cbb59530182d1b1c33f1095a50c2ee57
mamba <m.a.haery110@gmail.com>
2023-07-16 19:19:33 +0330

    added customer event handlers

---
+++ src/CrudTest.Application/Customers/Events/CustomerCreatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using CrudTest.Domain.CustomerAggregate.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CrudTest.Application.Customers.Events;
+internal sealed class CustomerCreatedEventHandler : INotificationHandler<CustomerCreatedEvent>
+{
+    private readonly ILogger<CustomerCreatedEventHandler> _logger;
+
+    public CustomerCreatedEventHandler(ILogger<CustomerCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(CustomerCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"CrudTest Domain Event: {notification.Customer.FirstName} {notification.Customer.LastName} Created.");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/CrudTest.Application/Customers/Events/CustomerUpdatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using CrudTest.Domain.CustomerAggregate.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CrudTest.Application.Customers.Events;
+internal sealed class CustomerUpdatedEventHandler : INotificationHandler<CustomerUpdatedEvent>
+{
+    private readonly ILogger<CustomerUpdatedEventHandler> _logger;
+
+    public CustomerUpdatedEventHandler(ILogger<CustomerUpdatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(CustomerUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"CrudTest Domain Event: {notification.Customer.FirstName} {notification.Customer.LastName} Information Updated.");
+
+        return Task.CompletedTask;
+    }
+}
